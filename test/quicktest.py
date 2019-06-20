a55ba7782d224cfee585294b1c9d140c624d9ad6
giles <gilesknap@gmail.com>
2019-06-20 11:51:13 +0100

    first pass python3 changes (significant reformatting)

---
+++ test/quicktest.py
@@ -0,0 +1,8 @@
+from dls_pmaclib.dls_pmacremote import PmacEthernetInterface
+
+pmac1 = PmacEthernetInterface(verbose=True)
+pmac1.setConnectionParams('172.23.240.97', 1025)
+pmac1.connect()
+print(pmac1.getNumberOfAxes())
+
+
