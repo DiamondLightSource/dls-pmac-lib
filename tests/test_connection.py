3e08c140427d3585b51cacb4d8c2b54ee2a1ed57
Bonnie McCallion <bem@observatorysciences.co.uk>
2021-11-08 17:57:32 +0000

    Updated unit tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -15,8 +15,23 @@ class TestRemotePmacInterface(unittest.TestCase):
         assert obj.hostname == "localhost"
         assert obj.port == None
 
+    @patch("dls_pmacremote.RemotePmacInterface._sendCommand", return_value = "response")
+    def test_send_command(self, mock_send_cmd):
+        obj = dls_pmacremote.RemotePmacInterface()
+        response, success = obj.sendCommand("test")
+        assert response == "response"
+        assert success == True
+
+    @patch("dls_pmacremote.RemotePmacInterface._sendCommand")
+    def test_send_command_null_error(self, mock_send_cmd):
+        mock_send_cmd.side_effect = IOError
+        obj = dls_pmacremote.RemotePmacInterface()
+        response, success = obj.sendCommand("test")
+        assert response == "I/O error during comm with PMAC: "
+        assert success == False
+
     @patch("dls_pmacremote.RemotePmacInterface")
-    def test_sendCommand(self, mock_sendcmd):
+    def test_send_command_not_implemented(self, mock_sendcmd):
         mock_instance = Mock()
         mock_instance._sendCommand.side_effect = NotImplementedError
         mock_sendcmd.return_value = mock_instance
@@ -24,7 +39,54 @@ class TestRemotePmacInterface(unittest.TestCase):
         with self.assertRaises(NotImplementedError):
             obj._sendCommand("command")
 
+    @patch("dls_pmacremote.RemotePmacInterface.sendCommand")
+    def test_get_pmac_model_code(self, mock_send_cmd):
+        mock_send_cmd.return_value = ("123456\r\x06", True)
+        obj = dls_pmacremote.RemotePmacInterface()
+        obj._pmacModelCode = None
+        assert obj.getPmacModelCode() == 123456
+
+    @patch("dls_pmacremote.RemotePmacInterface.getPmacModelCode")
+    def test_get_pmac_model(self, mock_get_code):
+        mock_get_code.return_value = 603382
+        obj = dls_pmacremote.RemotePmacInterface()
+        obj._pmacModelName = None
+        assert obj.getPmacModel() == "Geo Brick (3U Turbo PMAC2)"
+
+    @patch("dls_pmacremote.RemotePmacInterface.getPmacModelCode")
+    def test_get_pmac_model_short_name(self, mock_get_code):
+        mock_get_code.return_value = 603382
+        obj = dls_pmacremote.RemotePmacInterface()
+        obj._shortModelName = None
+        assert obj.getShortModelName() == "Geobrick"
+
+    @patch("dls_pmacremote.RemotePmacInterface.getIVars")
+    def test_get_num_macro_station_axes(self, mock_get_ivars):
+        mock_get_ivars.return_value = [1,2,3,4]
+        obj = dls_pmacremote.RemotePmacInterface()
+        assert obj._getNumberOfMacroStationAxes() == 32
+
+    '''@patch("dls_pmacremote.RemotePmacInterface.sendCommand")
+    def test_send_series(self, mock_send_cmd):
+        mock_send_cmd.return_value = ("repsonse",True)
+        obj = dls_pmacremote.RemotePmacInterface()
+        obj.sendSeries(["cmd1","cmd2","cmd3"])'''
+
+    @patch("dls_pmacremote.RemotePmacInterface.sendCommand")
+    def test_disable_limits_not_enabled_disable_true(self, mock_send_cmd):
+        mock_send_cmd.return_value = ("$820401\r\x06",True)
+        obj = dls_pmacremote.RemotePmacInterface()
+        cmd, retStr, status = obj.disableLimits(1,True)
+        assert cmd == ""
+        assert retStr == ""
+        assert status == False
+
 class TestSshConnection(unittest.TestCase):
+
+    @patch("dls_pmacremote.PPmacSshInterface.client")
+    def test_start_gpascii(self, mock_gpascii):
+        obj = dls_pmacremote.PPmacSshInterface()
+        obj.start_gpascii()
 
     def test_connection_already_open(self):
         obj = dls_pmacremote.PPmacSshInterface()
@@ -103,10 +165,18 @@ class TestSshConnection(unittest.TestCase):
         obj.disconnect()
         assert obj.isConnectionOpen == False
 
+    '''@patch("dls_pmacremote.PPmacSshInterface.sendCommand")
+    def test_get_pmac_model_code(self, mock_send_cmd):
+        mock_send_cmd.return_value = ("123456", True)
+        obj = dls_pmacremote.RemotePmacInterface()
+        obj._pmacModelCode = None
+        assert obj.getPmacModelCode() == 123456'''
+
     #@patch("paramiko.client.SSHClient.invoke_shell")
-    #def _sendCommand(self):#, mock_client):
-        #obj = dls_pmacremote.PPmacSshInterface()
-        #obj.gpascii_client = mock_client.return_value
+    #def test_sendCommand(self, mock_client):
+     #   mock_client.send.return_value = "n"
+      #  obj = dls_pmacremote.PPmacSshInterface()
+       # obj.gpascii_client = mock_client.return_value
         #obj._sendCommand("command")
 
     '''def test_get_file(self):
@@ -114,7 +184,6 @@ class TestSshConnection(unittest.TestCase):
         my_directory = ""
         obj = dls_pmacremote.PPmacSshInterface()
         obj.getFile(fake_file,my_directory)'''
-
 
     #def test_put_file()
 
@@ -286,6 +355,8 @@ class TestTelnetConnection(unittest.TestCase):
         expected_result = True
         self.assertEqual(actual_return, expected_return)
         self.assertEqual(actual_result, expected_result)
+
+    #def test_connect_exception(self):
 
     def test_disconnect_no_connection_open(self):
         obj = dls_pmacremote.PmacTelnetInterface()
---
+++ tests/test_dls_pmaclib.py
@@ -0,0 +1,26 @@
+import unittest
+import io
+import sys
+sys.path.append('/home/dlscontrols/bem-osl/dls-pmac-lib/dls_pmaclib')
+from dls_pmaclib import HelloClass, say_hello_lots
+from cli import main
+
+class DlsControlTest(unittest.TestCase):
+
+    def test_hello_class_formats_greeting(self):
+        obj = HelloClass("test")
+        assert obj.format_greeting() == "Hello test"
+
+    def test_hello_lots_defaults(self):
+        capOutput = io.StringIO()
+        sys.stdout = capOutput
+        say_hello_lots()
+        sys.stdout = sys.__stdout__
+        assert capOutput.getvalue() == "Hello me\n" * 5
+
+    def test_cli(self):
+        capOutput = io.StringIO()
+        sys.stdout = capOutput
+        main(["person", "--times=2"])        
+        sys.stdout = sys.__stdout__
+        assert capOutput.getvalue() == "Hello person\n" * 2
